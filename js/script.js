0f6970834578ae0341e8a3defb0f936303d9290c
rhubba4863 <rph_1994@yahoo.com>
2023-03-03 15:24:26 -0500

    Completed all necessary work

--- js/script.js
+++ js/script.js
@@ -8,26 +8,91 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `Quotes - Here we save famous quotes, along with known features about them
 ***/
+const quotes = [
+  {
+    quote: "The closer we are to danger, the farther we are from harm.",
+    source: "H.G. Pippin",
+    citation: "The Lord of the Rings: The Two Towers",
+    year: "2002"
+  },
+  {
+    quote: "When the Lord closes a door, somewhere he opens a window.",
+    source: "Maria",
+    citation: "The Sound of Music",
+    year: "1965"
+  },
+  {
+    quote: "Polish and refine whatever talents the Lord has given you. Go forth in life with a twinkle in your eye and a smile on your face, but with great and strong purpose in your heart",
+    source: "Gordan B. Hinckley",
+    citation: "General Conference",
+    year: "2001"
+  },
+  {
+    quote: "Turn your wounds into Wisdom.",
+    source: "Oprah Winfrey",
+    citation: "Wellesley College, Commencement Address",
+    year: "1997"
+  },
+  {
+    quote: "This is not the end; It is not even the beginning of the end, But it may be the end of the beginning.",
+    source: "Winston Churchill",
+    citation: "Mansion House",
+    year: "1942"
+  },
+  {
+    quote: "Do, or do not… there is no try.",
+    source: "Yoda",
+    citation: null,
+    year: null
+  } 
+];
 
+var newColor = document.body.style.backgroundColor = rgb(58, 193, 98);
+
+//lastQuote - is used to remember what quote was last presented to the use
+var lastQuote = 0;
+
+/***
+ * `getRandomQuote` - This function randomly decides what quote to present to the user, also checking double-checking so no quote is presented 2 times in a row.
+***/
+function getRandomQuote(){
+  var randomNumber = 0;
+  
+  do {
+    randomNumber = Math.floor(Math.random() * (quotes.length) );
+  } while(randomNumber == lastQuote);
+
+  //Saving the location of the quote now being returned for future use.
+  lastQuote = randomNumber;
+  
+  return quotes[randomNumber];
+}
+
+/***
+ * `printQuote` - This function sets up the HTML of the quote, including the year and citation if any are known to exist for that quote
+***/
+function printQuote(){
+  const randomQuote = getRandomQuote();
+  let htmlString = 
+  `<p class="quote">${randomQuote.quote}</p>
+  <p class="source">${randomQuote.source}`;
+  
+  if(randomQuote["citation"] !== null){
+    htmlString += `<span class="citation">${randomQuote.citation}</span>`;
+  }
+  if(randomQuote["year"] !== null){
+    htmlString += `<span class="year">${randomQuote.year}</span>`;
+  }
+  
+  htmlString += `</p>`;
+
+  document.getElementById('quote-box').innerHTML = htmlString; 
+}
 
 
 /***
- * `getRandomQuote` function
+ * This step finally clicks the print quote button click for the user to view a new quote
 ***/
-
-
-
-/***
- * `printQuote` function
-***/
-
-
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
-document.getElementById('load-quote').addEventListener("click", printQuote, false);
+ document.getElementById('load-quote').addEventListener("click", printQuote, false);
